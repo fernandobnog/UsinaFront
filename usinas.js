b666091562038663b0a4d09ecc6fbfd26ccd1cf0
Fernando Nogueira <fernando.bnog@outlook.com>
2021-10-07 12:35:11 -0300

    menu criado

--- usinas.js
+++ usinas.js
@@ -74,6 +74,11 @@ function listarUsina(){
     })
 }
 
-$("i").click(function () {
-    $("ul").toggleClass("open");
-  });
+function funcaoMenu() {
+    var x = document.getElementById("navDemo");
+    if (x.className.indexOf("w3-show") == -1) {
+      x.className += " w3-show";
+    } else { 
+      x.className = x.className.replace(" w3-show", "");
+    }
+  }
